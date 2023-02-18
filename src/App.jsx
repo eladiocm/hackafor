9b5ee03436f3e797235df358835247bca78edba6
Mateo2131 <goz.matu@gmail.com>
2023-02-18 11:11:02 -0300

    feat: add authentication with GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { supabase } from './lib/client'
 
 function App() {
+  const [user, setUser] = useState()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    async function getSession() {
+      const {
+        data: { user }
+      } = await supabase.auth.getUser()
+
+      setUser(user)
+    }
+
+    getSession()
+  }, [navigate])
+
+  const handleLogOut = async () => {
+    // Handle errors later. The SignOut function return a error
+    await supabase.auth.signOut()
+    navigate('/auth')
+  }
+
   return (
-    <Wrapper>
-      <Title>Hello World!</Title>
-    </Wrapper>
+    <div>
+      <h1>Home Page</h1>
+      {user ? (
+        <>
+          <button onClick={handleLogOut}>Log Out</button>
+          <div>
+            <p>Logged User Info</p>
+            <p>Username: {user?.user_metadata.user_name}</p>
+            <img
+              src={user?.user_metadata.avatar_url}
+              alt={user?.user_metadata.user_name + ' ' + 'Avatar'}
+            />
+          </div>
+        </>
+      ) : (
+        <Link to='/auth'>Sign In</Link>
+      )}
+    </div>
   )
 }
 
