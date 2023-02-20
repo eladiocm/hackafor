205c2abc2e388e8ed7d5fd161d53a3c42a63bfb1
Mateo2131 <goz.matu@gmail.com>
2023-02-20 17:31:35 -0300

    feat: add iconoir config

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,18 @@ import './index.css'
 // Router config
 import router from './routes'
 import { RouterProvider } from 'react-router-dom'
+import { IconoirProvider } from 'iconoir-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <IconoirProvider
+    iconProps={{
+      strokeWidth: 1,
+      width: '1.5rem',
+      height: '1.5rem' // 24px
+    }}
+  >
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </IconoirProvider>
 )
