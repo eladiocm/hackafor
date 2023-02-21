0e7e8167047b2722d659d658fe6b951d2b33376a
Mateo2131 <goz.matu@gmail.com>
2023-02-21 09:59:38 -0300

    refactor: move global styles to /styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Header } from './components/Header/Header'
 import { Projects } from './components/Projects/Projects'
-import './App.css'
 import { useGetUser } from './hooks/useGetUser'
+
+// CSS Styles
+import './styles/index.css'
+import './styles/normalize.css'
 
 function App() {
   const { user } = useGetUser()
